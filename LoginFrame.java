8b1ad0a7c67fb7801a82f87522c8d6c607abb134
Ahmed404error <160779925+Ahmed404error@users.noreply.github.com>
2024-02-21 22:03:56 +0200

    Add files via upload

---
+++ LoginFrame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ahmed
+ */
+class LoginFrame {
+
+    static void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
